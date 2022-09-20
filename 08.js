c8e97b7fcfe899c2683b0aae15f227c630c67306
illy22 <illypendlyshak@gmail.com>
2022-09-20 18:15:04 +0300

    c

--- 08.js
+++ 08.js
@@ -1,3 +1,9 @@
-//alalalalalaalla//
-/*alalalalalalalalalala
-alalal*/
+// Оголошення змінної
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Призначення змінної
+studlyCapVar = 10;
+properCamelCase = 'A String';
+titleCaseOver = 9000;
